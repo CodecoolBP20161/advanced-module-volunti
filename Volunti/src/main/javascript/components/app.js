057fee0479a7a997d6cf503de080b3987e9f52b3
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-02-07 21:26:13 +0100

    finally a working setup :)

---
+++ Volunti/src/main/javascript/components/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <h1>Sample Text</h1>
+    </div>
+);
+
+export default App;
--- Volunti/src/main/javascript/index.js
+++ Volunti/src/main/javascript/index.js
@@ -4,11 +4,12 @@ import ReactDom from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux'
 import allReducers from './reducers';
+import App from './components/app';
 
 const store = createStore(allReducers);
 
 ReactDom.render(
-    <Provider store="{store}">
+    <Provider store={ store }>
         <App />
     </Provider>
     , document.getElementById('root'));
