be1a9c74f01f0b929aed9ebb819790c297dd5de8
matheisson <csanyil@gmail.com>
2017-02-09 18:41:37 +0100

    Fixed th:each on backend, objects stored in a list

--- Volunti/src/main/java/com/codecool/volunti/controller/MainController.java
+++ Volunti/src/main/java/com/codecool/volunti/controller/MainController.java
@@ -4,7 +4,9 @@ import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -14,14 +16,19 @@ public class MainController {
         return "index";
     }
 
-    // for tests
+    // for tests, but route and return value legit for the final product
     @GetMapping("/org/{id}/opp")
     public String display(HttpServletRequest req){
+        // equals find all
+        List<Opportunity> list = new ArrayList<>();
+
         Opportunity x = new Opportunity();
         Opportunity y = new Opportunity();
+        list.add(x);
+        list.add(y);
+
         y.title = "other desc";
-        req.setAttribute("opps", x);
-        req.setAttribute("opps", y);
+        req.setAttribute("opps", list);
         return "org_opportunities";
     }
 
