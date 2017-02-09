8b2d33b27b28a0cf7d03e0414451d4014ab82a34
matheisson <csanyil@gmail.com>
2017-02-09 17:47:50 +0100

    Implemented org/opp route and filled with test objects. Also added Opportunity class for testing

--- Volunti/src/main/java/com/codecool/volunti/controller/MainController.java
+++ Volunti/src/main/java/com/codecool/volunti/controller/MainController.java
@@ -1,7 +1,10 @@
 package com.codecool.volunti.controller;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class MainController {
@@ -10,4 +13,29 @@ public class MainController {
     public String getRoute(){
         return "index";
     }
+
+    // for tests
+    @GetMapping("/org/{id}/opp")
+    public String display(HttpServletRequest req){
+        Opportunity x = new Opportunity();
+        Opportunity y = new Opportunity();
+        y.title = "other desc";
+        req.setAttribute("opps", x);
+        req.setAttribute("opps", y);
+        return "org_opportunities";
+    }
+
+    // for tests
+    private class Opportunity {
+        public String title = "Test Title";
+        public String desc = "test description";
+        public int reqs = 4;
+        public Date from = new Date();
+        public Date to = new Date();
+
+        public Opportunity(){
+
+        }
+    }
+
 }
