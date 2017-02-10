72b351e6780de208351d8498a3da9d3e1ceac7d9
b-franka <b.franku@gmail.com>
2017-02-10 10:53:38 +0100

    organisation base class for testing

---
+++ Volunti/src/main/java/com/codecool/volunti/models/Organisation.java
@@ -0,0 +1,22 @@
+package com.codecool.volunti.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Data
+public class Organisation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String something;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "organisation", fetch = FetchType.EAGER)
+    private List<Opportunity> opportunities;
+
+}
