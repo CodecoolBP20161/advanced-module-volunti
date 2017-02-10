84d4d484d6a43c9a9815d654b929bd772a8077a6
lombocska <lombos.monika@gmail.com>
2017-02-10 12:10:02 +0100

    Added logging

--- Volunti/src/main/java/com/codecool/volunti/VoluntiApplication.java
+++ Volunti/src/main/java/com/codecool/volunti/VoluntiApplication.java
@@ -1,15 +1,56 @@
 package com.codecool.volunti;
 
+import com.codecool.volunti.model.Organisation;
 import com.codecool.volunti.model.User;
+import com.codecool.volunti.model.Volunteer;
+import com.codecool.volunti.repository.OrganisationRepository;
+import com.codecool.volunti.repository.UserRepository;
+import com.codecool.volunti.repository.VolunteerRepository;
+import com.codecool.volunti.service.DataLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class VoluntiApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(VoluntiApplication.class);
+
+	@Autowired
+	DataLoader dataLoader;
+
+	@Autowired
+	OrganisationRepository organisationRepository;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	VolunteerRepository volunteerRepository;
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(VoluntiApplication.class, args);
-
 	}
+
+	@PostConstruct
+	void seeData() {
+		LOGGER.info("seeData method called...");
+		for(Organisation organisation : organisationRepository.findAll()) {
+			LOGGER.info(organisation.toString());
+		}
+
+		for (User user : userRepository.findAll()) {
+			LOGGER.info(user.toString());
+		}
+
+		for (Volunteer volunteer : volunteerRepository.findAll()) {
+			LOGGER.info(volunteer.toString());
+		}
+	}
+
 }
