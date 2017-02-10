37d42daf739300f93c6427dc99619f4083828dc7
matheisson <csanyil@gmail.com>
2017-02-10 11:02:38 +0100

    Synchronized test object with final database entity, Description removed

--- Volunti/src/main/java/com/codecool/volunti/controller/MainController.java
+++ Volunti/src/main/java/com/codecool/volunti/controller/MainController.java
@@ -27,22 +27,38 @@ public class MainController {
         list.add(x);
         list.add(y);
 
-        y.title = "other desc";
+        y.title = "other title";
         req.setAttribute("opps", list);
         return "org_opportunities";
     }
 
     // for tests
     private class Opportunity {
-        public String title = "Test Title";
-        public String desc = "test description";
-        public int reqs = 4;
-        public Date from = new Date();
-        public Date to = new Date();
+        public int id = 0;
+        public String title = "Title";
+        public String accomodationType = "tent";
+        public String foodType = "halal";
+        public int hoursexpected = 100;
+        public String hoursExpectedType = "hrs";
+        public int minimumStayInDays = 2;
+        public Date availabilityFrom = new Date();
+        public Date dateAvailabilityTo = new Date();;
+        public String costs = "százezer";
+        public String requirements = "szükségletek";
+        public int numberOfVolunteers = 23423;
 
         public Opportunity(){
-
+            this.id++;
         }
+
+
+//        Id not null, int, serial, primary key
+//        title String
+//        number_of_volunteers int
+//        accomodation_type String
+//        food_type String
+//        hours_expected  int
+
     }
 
 }
