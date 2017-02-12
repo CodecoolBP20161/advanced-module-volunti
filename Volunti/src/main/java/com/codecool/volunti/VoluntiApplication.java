04a6689814fde8f79bd5820736908134be5ea849
lombocska <lombos.monika@gmail.com>
2017-02-12 19:29:32 +0100

    Added  EmbbedServletControllerCustomizer @Bean

--- Volunti/src/main/java/com/codecool/volunti/VoluntiApplication.java
+++ Volunti/src/main/java/com/codecool/volunti/VoluntiApplication.java
@@ -12,45 +12,56 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class VoluntiApplication {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(VoluntiApplication.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(VoluntiApplication.class);
 
-	@Autowired
-	DataLoader dataLoader;
+    @Autowired
+    DataLoader dataLoader;
 
-	@Autowired
-	OrganisationRepository organisationRepository;
+    @Autowired
+    OrganisationRepository organisationRepository;
 
-	@Autowired
-	UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-	@Autowired
-	VolunteerRepository volunteerRepository;
+    @Autowired
+    VolunteerRepository volunteerRepository;
 
-	public static void main(String[] args)
-	{
-		SpringApplication.run(VoluntiApplication.class, args);
-	}
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return (container -> {
+            ErrorPage custom404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error");
+            container.addErrorPages(custom404Page);
+        });
+    }
 
-	@PostConstruct
-	void seeData() {
-		LOGGER.info("seeData method called...");
-		for(Organisation organisation : organisationRepository.findAll()) {
-			LOGGER.info(organisation.toString());
-		}
+    public static void main(String[] args) {
+        SpringApplication.run(VoluntiApplication.class, args);
+    }
 
-		for (User user : userRepository.findAll()) {
-			LOGGER.info(user.toString());
-		}
+    @PostConstruct
+    void seeData() {
+        LOGGER.info("seeData method called...");
+        for (Organisation organisation : organisationRepository.findAll()) {
+            LOGGER.info(organisation.toString());
+        }
 
-		for (Volunteer volunteer : volunteerRepository.findAll()) {
-			LOGGER.info(volunteer.toString());
-		}
-	}
+        for (User user : userRepository.findAll()) {
+            LOGGER.info(user.toString());
+        }
+
+        for (Volunteer volunteer : volunteerRepository.findAll()) {
+            LOGGER.info(volunteer.toString());
+        }
+    }
 
 }
