d7c1ef7a5a2013aa24e2cda72d44ebab4a4ac3e6
BTamara <bolyatami@gmail.com>
2017-02-13 12:03:25 +0100

    save organisation and user into database

--- Volunti/src/main/java/com/codecool/volunti/controller/RegistrationController.java
+++ Volunti/src/main/java/com/codecool/volunti/controller/RegistrationController.java
@@ -1,12 +1,18 @@
 package com.codecool.volunti.controller;
 
 
+import com.codecool.volunti.model.Organisation;
 import com.codecool.volunti.model.User;
+import com.codecool.volunti.repository.OrganisationRepository;
+import com.codecool.volunti.repository.UserRepository;
 import com.codecool.volunti.service.NotificationService;
+import com.codecool.volunti.service.OrganisationService;
+import com.codecool.volunti.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +20,40 @@ public class RegistrationController {
 
     private Logger LOGGER = LoggerFactory.getLogger(RegistrationController.class);
 
+    private OrganisationRepository organisationRepository;
+    private OrganisationService organisationService;
+    private UserService userService;
+
     @Autowired
     private NotificationService notificationService;
+
+    //render organisation registration
+    @RequestMapping( value = "/registration/organisation/step1", method = RequestMethod.GET )
+    public String step1() {
+        return "registration/organisation/step1";
+    }
+
+    //save organisation registration
+    @RequestMapping( value = "/registration/organisation/step1", method = RequestMethod.POST )
+    public String saveStep1(Organisation organisation) {
+        Organisation savedOrganisation = organisationService.saveOrganisation(organisation);
+        LOGGER.info("savedOrganisation: " + savedOrganisation);
+        return "redirect:/registration/organisation/step2";
+    }
+
+    //render user registration
+    @RequestMapping( value = "/registration/organisation/step2", method = RequestMethod.GET )
+    public String step2() {
+        return "registration/user";
+    }
+
+    //save user registration
+    @RequestMapping( value = "/registration/organisation/step2", method = RequestMethod.POST )
+    public String save(User user) {
+        User savedUser = userService.saveUser(user);
+        LOGGER.info("savedUser: " + savedUser);
+        return "/registration/organisation/confirmation";   //Should I redirect somewhere else?
+    }
 
     @RequestMapping("/signup-success")
     public String signupSuccess(){
@@ -36,5 +74,8 @@ public class RegistrationController {
 
         return "Thank you for registering with us.";
     }
+
+
+
 
 }
---
+++ Volunti/src/main/java/com/codecool/volunti/service/UserService.java
@@ -0,0 +1,25 @@
+package com.codecool.volunti.service;
+
+
+import com.codecool.volunti.model.User;
+import com.codecool.volunti.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService( UserRepository userRepository){
+
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser( User user){
+
+        return userRepository.save(user);
+    }
+
+}
