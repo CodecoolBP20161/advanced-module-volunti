af858ec63510798832408a204c7f6edfd7c8c05f
b-franka <b.franku@gmail.com>
2017-02-20 11:48:40 +0100

    modified form method

--- Volunti/src/main/java/com/codecool/volunti/controller/OpportunityController.java
+++ Volunti/src/main/java/com/codecool/volunti/controller/OpportunityController.java
@@ -26,23 +26,24 @@ public class OpportunityController {
     @Autowired
     private OrganisationRepository organisationRepository;
 
-    @GetMapping("/{organisation_id}/opp/form")
+    @GetMapping("/{organisation_id}/opportunity/new")
     public String form(@PathVariable Integer organisation_id, Model model){
 
         Organisation organisation;
         organisation = organisationRepository.findByOrganisationId(organisation_id);
 
-        Opportunity opportunity;
-       opportunity = new Opportunity(organisation, "First opportunity", 10, "Tent",
-              "Vega", 3, "none", 2,
-              new java.sql.Date(2017 - 02 - 16), new java.sql.Date(2017 - 02 - 21), "free", "English");
-        model.addAttribute("opportunity", opportunity);
+//        Opportunity opportunity;
+//        opportunity = new Opportunity(organisation, "First opportunity", 10, "Tent",
+//              "Vega", 3, "none", 2,
+//              new java.sql.Date(2017 - 02 - 16), new java.sql.Date(2017 - 02 - 21), "free", "English");
+//        model.addAttribute("opportunity", opportunity);
+        model.addAttribute("organisation", organisation);
         return "multi-form";
     }
 
     @PostMapping("/{organisation_id}/opportunity/new")
     public String saveOpportunity(@ModelAttribute @Valid Opportunity opportunity){
-        //log.info("opportunity = " + opportunity);
+        log.info("opportunity = " + opportunity);
         opportunityRepository.save(opportunity);
         return "redirect:organisation_opportunities.html";
     }
@@ -53,30 +54,37 @@ public class OpportunityController {
 
         Organisation organisation;
         organisation = organisationRepository.findByOrganisationId(organisation_id);
+        log.info("Organisation found: " + organisation);
 
-        List<Opportunity> opportunities;
-        opportunities = opportunityRepository.findByOrganisation(organisation);
+
 
         Opportunity opportunity;
         opportunity = new Opportunity(organisation, "First opportunity", 10, "Tent",
                 "Vega", 3, "none", 2,
                 new java.sql.Date(2017 - 02 - 16), new java.sql.Date(2017 - 02 - 21), "free", "English");
         opportunityRepository.save(opportunity);
+
+        List<Opportunity> opportunities;
+        opportunities = opportunityRepository.findByOrganisation(organisation);
+        log.info("Opportunities found: " + opportunities);
+
         model.addAttribute("opportunities", opportunities);
         model.addAttribute("organisation", organisation);
         return "organisation_opportunities";
     }
 
     @GetMapping("/{organisation_id}/opportunity/delete/{opportunity_id}")
-    public String deleteOpportunity(@PathVariable Integer organisation_id, Integer opportunity_id){
+    public String deleteOpportunity(@PathVariable Integer organisation_id, @PathVariable Integer opportunity_id){
         opportunityRepository.delete(opportunity_id);
         return "redirect:organisation_opportunities.html";
     }
 
-    @PostMapping("/{organisation_id}/opportunity/edit/{opportunity_id}")
-    public String editOpportunity(@PathVariable Integer organisation_id, Integer opportunity_id) {
-        opportunityRepository.findById(opportunity_id);
-        return "redirect:organisation_opportunities.html";
+    @GetMapping("/{organisation_id}/opportunity/edit/{opportunity_id}")
+    public String editOpportunity(@PathVariable Integer organisation_id,@PathVariable Integer opportunity_id, Model model) {
+        Opportunity opportunity = opportunityRepository.findById(opportunity_id);
+        model.addAttribute("opportunity", opportunity);
+        log.info("opp: " + opportunity);
+        return "multi-form";
     }
 
 }
