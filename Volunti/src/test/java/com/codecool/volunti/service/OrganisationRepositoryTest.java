1b4fb4f161988cff055312c3ac162394c0b8316b
BTamara <bolyatami@gmail.com>
2017-02-20 14:22:35 +0100

    integration test setup refactor and happy path added

--- Volunti/src/test/java/com/codecool/volunti/service/OrganisationRepositoryTest.java
+++ Volunti/src/test/java/com/codecool/volunti/service/OrganisationRepositoryTest.java
@@ -3,29 +3,48 @@ package com.codecool.volunti.service;
 import com.codecool.volunti.model.*;
 import com.codecool.volunti.model.enums.*;
 import com.codecool.volunti.repository.OrganisationRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+@Transactional
 public class OrganisationRepositoryTest extends AbstractServiceTest {
 
     @Autowired
     private OrganisationRepository repository;
+    private Organisation organisation;
 
-    @Test
-    public void testExample() throws Exception {
+    @Before
+    public void setUp() {
         ArrayList<SpokenLanguage> spokenLanguages = new ArrayList<>();
         spokenLanguages.add(SpokenLanguage.ENGLISH);
         spokenLanguages.add(SpokenLanguage.HUNGARIAN);
 
-        Organisation org = new Organisation("Test 1", Category.TEACHING, "Country", "zipcode", "City", "Address", spokenLanguages, "Mission minimum 10 character", "Desc 1 min 3 character", "Desc 2 min 3 character");
-        this.repository.save(org);
-        org = this.repository.findByName("Test 1");
-        assertThat(org.getName()).isEqualTo("Test 1");
-        assertThat(org.getCategory()).isEqualTo(Category.TEACHING);
+        organisation = new Organisation("Test 1", Category.TEACHING, "Country", "zipcode", "City", "Address", spokenLanguages, "Mission minimum 10 character", "Desc 1 min 3 character", "Desc 2 min 3 character");
+        this.repository.save(organisation);
+    }
+
+    @Test
+    public void testForTheFields() {
+
+        organisation = this.repository.findByName("Test 1");
+        assertThat(organisation.getName()).isEqualTo("Test 1");
+        assertThat(organisation.getCategory()).isEqualTo(Category.TEACHING);
+        assertThat(organisation.getCountry()).isEqualTo("Country");
+        assertThat(organisation.getZipcode()).isEqualTo("zipcode");
+        assertThat(organisation.getCity()).isEqualTo("City");
+        assertThat(organisation.getAddress()).isEqualTo("Address");
+        assertThat(organisation.getSpokenLanguage().toString()).isEqualTo("[ENGLISH, HUNGARIAN]");
+        assertThat(organisation.getMission()).isEqualTo("Mission minimum 10 character");
+        assertThat(organisation.getDescription1()).isEqualTo("Desc 1 min 3 character");
+        assertThat(organisation.getDescription2()).isEqualTo("Desc 2 min 3 character");
+
     }
 
 }
