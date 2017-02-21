c26d7714fe8f717e038676320faf550ac132fc86
b-franka <b.franku@gmail.com>
2017-02-21 14:31:48 +0100

    populated skills data in Dataloader.java

--- Volunti/src/main/java/com/codecool/volunti/service/DataLoader.java
+++ Volunti/src/main/java/com/codecool/volunti/service/DataLoader.java
@@ -1,16 +1,10 @@
 package com.codecool.volunti.service;
 
 
-import com.codecool.volunti.model.Opportunity;
-import com.codecool.volunti.model.Organisation;
-import com.codecool.volunti.model.User;
-import com.codecool.volunti.model.Volunteer;
+import com.codecool.volunti.model.*;
 import com.codecool.volunti.model.enums.Category;
 import com.codecool.volunti.model.enums.SpokenLanguage;
-import com.codecool.volunti.repository.OpportunityRepository;
-import com.codecool.volunti.repository.OrganisationRepository;
-import com.codecool.volunti.repository.UserRepository;
-import com.codecool.volunti.repository.VolunteerRepository;
+import com.codecool.volunti.repository.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -29,13 +24,16 @@ public class DataLoader {
     private UserRepository userRepository;
     private VolunteerRepository volunteerRepository;
     private OpportunityRepository opportunityRepository;
+    private SkillRepository skillRepository;
 
     @Autowired
-    public DataLoader(OrganisationRepository organisationRepository, UserRepository userRepository, VolunteerRepository volunteerRepository, OpportunityRepository opportunityRepository) {
+    public DataLoader(OrganisationRepository organisationRepository, UserRepository userRepository, VolunteerRepository volunteerRepository,
+                      OpportunityRepository opportunityRepository, SkillRepository skillRepository) {
         this.organisationRepository = organisationRepository;
         this.userRepository = userRepository;
         this.volunteerRepository = volunteerRepository;
         this.opportunityRepository = opportunityRepository;
+        this.skillRepository = skillRepository;
     }
 
     @PostConstruct
@@ -55,12 +53,32 @@ public class DataLoader {
                 "Vega", 3, "none", 2,
                 new java.sql.Date(2017 - 02 - 16), new java.sql.Date(2017 - 02 - 21), "free", "English");
 
+
         userRepository.save(user1);
         organisationRepository.save(organisation1);
         volunteerRepository.save(volunteer);
         opportunityRepository.save(opportunity);
         opportunityRepository.save(opportunity1);
+        loadSkills();
         LOGGER.info("loadData method called ...");
+    }
+
+    private void loadSkills() {
+        List<Skill> skills = new ArrayList<>();
+        skills.add(new Skill("General Maintenance"));
+        skills.add(new Skill("Language practice"));
+        skills.add(new Skill("Teaching"));
+        skills.add(new Skill("Yoga"));
+        skills.add(new Skill("Cooking"));
+        skills.add(new Skill("Nursing"));
+        skills.add(new Skill("Communication"));
+        skills.add(new Skill("Programming"));
+        skills.add(new Skill("Babysitting"));
+        skills.add(new Skill("Farming"));
+        skills.add(new Skill("Building"));
+        skills.add(new Skill("Gardening"));
+
+        skillRepository.save(skills);
     }
 }
 
