63002d4d5e5601138566ccce5a13c3a7dac10d61
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-02-21 15:44:14 +0100

    removed Async annotations

--- Volunti/src/main/java/com/codecool/volunti/service/OrganisationService.java
+++ Volunti/src/main/java/com/codecool/volunti/service/OrganisationService.java
@@ -4,6 +4,7 @@ package com.codecool.volunti.service;
 import com.codecool.volunti.model.Organisation;
 import com.codecool.volunti.repository.OrganisationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,7 +27,7 @@ public class OrganisationService {
         return organisationRepository.findOne(id);
     }
 
-    public Organisation get(String name) {
+    public Organisation getByName(String name) {
         return organisationRepository.findByName(name);
     }
 
