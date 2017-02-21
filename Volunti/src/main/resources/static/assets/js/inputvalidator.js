e130c45c17e24bf537815d381c2339de4cb22948
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-02-21 18:56:16 +0100

    email and org. name validation implemented

--- Volunti/src/main/resources/static/assets/js/inputvalidator.js
+++ Volunti/src/main/resources/static/assets/js/inputvalidator.js
@@ -1,29 +1,40 @@
 $(document).ready(function() {
-    var inputField = $('#organizationName');
-    inputField
-        .keyup(function() {
-            var value = inputField.val();
-            $.ajax({
-                url: "/registration/ValidateFieldIfExists",
-                type: 'POST',
-                async: true,
-                contentType: "application/json",
-                  data:  JSON.stringify({
-                      "entityName": "organisation",
-                      "fieldName": "name",
-                      "value": value
-                  }),
-                success: function(exists){
-                    console.log(exists);
-                    var InputSpan = $('#organizationNameSpan');
-                    console.log(exists);
-                    if(exists == "true") {
-                        InputSpan.text(inputField.val() + " is already used.");
-                        InputSpan.css("display", "block");
-                    }else if(exists == "false"){
-                        InputSpan.css("display", "none");
+
+    //Organization input field
+    var addValidateEvent = function (inputID, spanID, buttonID, entityName, fieldName) {
+        var inputField = $('#' + inputID);
+        inputField
+            .keyup(function () {
+                var value = inputField.val();
+                $.ajax({
+                    url: "/registration/ValidateFieldIfExists",
+                    type: 'POST',
+                    async: true,
+                    contentType: "application/json",
+                    data: JSON.stringify({
+                        "entityName": entityName,
+                        "fieldName": fieldName,
+                        "value": value
+                    }),
+                    success: function (exists) {
+                        console.log(exists);
+                        var InputSpan = $('#' + spanID);
+                        var submitButton = $('#' + buttonID);
+                        console.log(exists);
+                        if (exists == "true") {
+                            InputSpan.text(inputField.val() + " is already in use.");
+                            InputSpan.css("display", "block");
+                            submitButton.prop('disabled', true);
+
+                        } else if (exists == "false") {
+                            InputSpan.css("display", "none");
+                            submitButton.prop('disabled', false);
+                        }
                     }
-                }
-            })
-        })
+                })
+            });
+    };
+
+    addValidateEvent("organizationName", "organizationNameSpan", "submitButton", "organisation", "name");
+    addValidateEvent("userEmailInput", "userEmailSpan", "submitButton", "user", "email");
 });
