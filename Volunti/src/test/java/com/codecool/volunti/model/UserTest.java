e4580df2164a43abe5286e334a71a639afeb5c6b
ppayter-cc <pomsarpeter@gmail.com>
2017-02-25 17:08:03 +0100

    tests for setSalt & hashPassword methods in User model

--- Volunti/src/test/java/com/codecool/volunti/model/UserTest.java
+++ Volunti/src/test/java/com/codecool/volunti/model/UserTest.java
@@ -1,11 +1,46 @@
 package com.codecool.volunti.model;
 
-import lombok.Data;
-import com.codecool.volunti.model.User;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class UserTest {
+
+    private Logger LOGGER = LoggerFactory.getLogger(UserTest.class);
+
+    private User testUser;
+
+    @Before
+    public void setUp() throws Exception {
+        testUser = new User("firstName", "lastName", "email@mail.com", "password", null, null);
+    }
+
+    @Test
+    public void setSalt() throws Exception {
+        LOGGER.info("salt: {}", testUser.getSalt());
+        assertNotNull(testUser.getSalt());
+    }
+
+    @Test
+    public void hashPassword() throws Exception {
+        String testHash = BCrypt.hashpw("password", testUser.getSalt());
+        LOGGER.info("testHash: {}", testHash);
+
+        String testUserPassword;
+        testUserPassword = testUser.getPassword();
+        LOGGER.info("testUser.password before hashing: {}", testUserPassword);
+
+        testUser.hashPassword(testUser.getPassword());
+        testUserPassword = testUser.getPassword();
+        LOGGER.info("testUser.password after hashing {}", testUserPassword);
+
+        assertEquals(testHash, testUserPassword);
+    }
 
     @Test
     public void testUserStatus() {
