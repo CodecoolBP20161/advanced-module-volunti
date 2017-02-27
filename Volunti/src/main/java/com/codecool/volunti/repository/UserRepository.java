306cccccde2169d647a29449e7817bc6b4a2a19a
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-02-27 11:09:53 +0100

    added findByActivationID() function

--- Volunti/src/main/java/com/codecool/volunti/repository/UserRepository.java
+++ Volunti/src/main/java/com/codecool/volunti/repository/UserRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByEmail(String Email);
+    User findByActivationID(UUID activationID);
 
 }
