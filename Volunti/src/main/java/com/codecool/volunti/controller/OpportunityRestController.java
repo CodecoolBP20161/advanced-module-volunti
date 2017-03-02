00ebf0d25db73149312774db262bd8164bb0c88d
Ivanics Szilárd <ivanics.szilard@gmail.com>
2017-03-02 09:51:24 +0100

    get filter elemets from restController

--- Volunti/src/main/java/com/codecool/volunti/controller/OpportunityRestController.java
+++ Volunti/src/main/java/com/codecool/volunti/controller/OpportunityRestController.java
@@ -2,20 +2,18 @@ package com.codecool.volunti.controller;
 
 
 import com.codecool.volunti.model.Opportunity;
+import com.codecool.volunti.model.Organisation;
 import com.codecool.volunti.model.Skill;
 import com.codecool.volunti.model.enums.Category;
 import com.codecool.volunti.repository.OpportunityRepository;
+import com.codecool.volunti.repository.OrganisationRepository;
 import com.codecool.volunti.repository.SkillRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 //import com.codecool.volunti.service.FilterService;
 
@@ -29,6 +27,9 @@ public class OpportunityRestController {
 
     @Autowired
     private SkillRepository skillRepository;
+
+    @Autowired
+    private OrganisationRepository organisationRepository;
 
     @GetMapping("/categ")
     public @ResponseBody EnumSet<Category> findCategories() {
@@ -44,20 +45,46 @@ public class OpportunityRestController {
             System.out.println(c.toString());
         }
         System.out.println(enumList.get(2));
-
         return enumSet;
     }
 
-    @GetMapping("/find")
+    //ContentNegotiatingViewResolver
+    //ContentNegotiationManager
+    @RequestMapping(value="/find",
+                    method= RequestMethod.GET
+                    )
     public   @ResponseBody List<Opportunity> findOpportunities() {
         log.info("opportunityRepository.findAll()");
-        return (List<Opportunity>) opportunityRepository.findAll();
+        List<Opportunity> allOpportunity = (List<Opportunity>) opportunityRepository.findAll();
+        int size = allOpportunity.size();
+        return allOpportunity;
     }
 
     @GetMapping("/skill")
-    public @ResponseBody List<Skill> findSkills() {
-        log.info("opportunityRepository.findAll()");
-        return (List<Skill>) skillRepository.findAll();
+    public @ResponseBody Map<String, List<String>> findSkills() {
+        List<Skill> skills = (List<Skill>) skillRepository.findAll();
+        Map<String, List<String>> skillNames = new HashMap<>();
+        skillNames.put("skills", skills.stream().map(s -> s.getName()).collect(Collectors.toList()));
+        log.info("SkillNames" + String.valueOf(skillNames));
+        return skillNames;
+    }
+
+    @GetMapping("/category")
+    public @ResponseBody Map<String, Set<String>> findCategory() {
+        List<Organisation> skills = (List<Organisation>) organisationRepository.findAll();
+        Map<String, Set<String>> skillNames = new HashMap<>();
+        skillNames.put("categories", skills.stream().map(o -> o.getCategory().name()).collect(Collectors.toSet()));
+        log.info("SkillNames" + String.valueOf(skillNames));
+        return skillNames;
+    }
+
+    @GetMapping("/location")
+    public @ResponseBody Map<String, Set<String>> findLocation() {
+        List<Organisation> skills = (List<Organisation>) organisationRepository.findAll();
+        Map<String, Set<String>> skillNames = new HashMap<>();
+        skillNames.put("categories", skills.stream().map(Organisation::getCountry).collect(Collectors.toSet()));
+        log.info("SkillNames" + String.valueOf(skillNames));
+        return skillNames;
     }
 
     //time, location, skill, category
