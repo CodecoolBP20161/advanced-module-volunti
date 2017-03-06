08225978f2b24861d171d0b568d70003518836fd
gamte19 <gamte19@gmail.com>
2017-03-06 20:26:06 +0100

    Skill, category, location filters work with data view

--- Volunti/src/main/frontendA/src/main.jsx
+++ Volunti/src/main/frontendA/src/main.jsx
@@ -2,6 +2,29 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+var url = "http://localhost:8080/api/opportunities/find/1?from=2020-10-10&to=1999-10-10&location&skills&category&pageSize=10";
+var location = "";
+var skill = "";
+var category = "";
+
+var updateUrl= function () {
+    url = "http://localhost:8080/api/opportunities/find/1?from=2020-10-10&to=1999-10-10&pageSize=10";
+    if (location != ""){
+        url += "&location=" + location;
+    } else {
+        url += "&location";
+    }
+    if (skill != ""){
+        url += "&skills=" + skill;
+    } else {
+        url += "&skills";
+    }
+    if (category != ""){
+        url += "&category=" + category;
+    } else {
+        url += "&category";
+    }
+}
 
 var Option = React.createClass({
     render: function () {
@@ -17,14 +40,53 @@ var Filter = React.createClass({
             options.push(<Option option={option} key={i}/>)
         });
         return(<div className="col-sm-3">
-                    <div className="field custom-select-box">
-                        {/*<h5>Filter by: {this.props.label}</h5>*/}
-                        <select title="sort">
-                            <option defaultValue={this.props.label}>{this.props.label}(optional)</option>
-                            {options}</select>
-                    </div>
-                </div>)
+            <div className="field custom-select-box" id={this.props.label}>
+                {/*<h5>Filter by: {this.props.label}</h5>*/}
+                <select title="sort" onChange={this.handleChange}>
+                    <option defaultValue={this.props.label}>{this.props.label}(optional)</option>
+                    {options}</select>
+            </div>
+        </div>)
+    },
+
+    handleChange: function (e) {
+
+        var name = e.target.value;
+        //console.log(name);
+        //console.log(e.target.parentNode.id);
+
+        var type = e.target.parentNode.id;
+        var value = e.target.value;
+
+        switch(type) {
+            case "Location":
+                location = value;
+                if(value.includes("optional")){
+                    location = "";
+                }
+                break;
+            case "Skill":
+                skill = value;
+                if(value.includes("optional")) {
+                    skill = "";
+                }
+                break;
+
+            case "Category":
+                category = value;
+                if(value.includes("optional")) {
+                    category = "";
+                }
+                break;
+            default:
+                //console.log(type);
+                break;
+        };
+
+        updateUrl();
     }
+
+
 });
 
 
@@ -59,15 +121,24 @@ var Filters = React.createClass({
 });
 
 
+
 var Table = React.createClass({
 
     loadOpportunityFromServer: function () {
         var self = this;
         $.ajax({
-            url: "http://localhost:8080/api/opportunities/find/all/1"
+            url: url
         }).then(function (data) {
             self.setState({opportunities: data.result});
         });
+    },
+
+    updateIfNeeded: function () {
+        var isNeeded = this.state.url != url;
+        this.state.url = url;
+        if (isNeeded) {
+            this.loadOpportunityFromServer();
+        }
     },
 
     getInitialState: function () {
@@ -75,13 +146,18 @@ var Table = React.createClass({
     },
 
     componentDidMount: function () {
+        this.state.url = url;
         this.loadOpportunityFromServer();
+        setInterval(this.updateIfNeeded, 1000);
     },
 
     render() {
         return ( <OpportunityTable opportunities={this.state.opportunities}/> );
     }
 });
+
+
+
 
 var Opportunity = React.createClass({
     getInitialState: function() {
@@ -93,23 +169,23 @@ var Opportunity = React.createClass({
         else return (
             <tr>
                 <td>
-                    <h4><b>{this.props.opportunity.title}</b></h4>
+                    <h5><b>{this.props.opportunity.title}</b></h5>
                 </td>
                 <td className="col-xs-3 n-p-r">
-                    <span><h4><b>{this.props.opportunity.dateAvailabilityTo}</b></h4></span>
-                    <span><h4><b>{this.props.opportunity.availabilityFrom}</b></h4></span>
+                    <h5><b>{this.props.opportunity.dateAvailabilityTo}</b></h5>
+                    <h5><b>{this.props.opportunity.availabilityFrom}</b></h5>
                 </td>
                 <td>
-                    <h4><b>{this.props.opportunity.foodType}</b></h4>
+                    <h5><b>{this.props.opportunity.foodType}</b></h5>
                 </td>
                 <td>
-                    <h4><b>{this.props.opportunity.skills}</b></h4>
+                    <h5><b>{this.props.opportunity.skills}</b></h5>
                 </td>
                 <td>
-                    <h4><b>{this.props.opportunity.hoursExpected}</b></h4>
+                    <h5><b>{this.props.opportunity.hoursExpected}</b></h5>
                 </td>
                 <td>
-                    <h4><b>{this.props.opportunity.costs}</b></h4>
+                    <h5><b>{this.props.opportunity.costs}</b></h5>
                 </td>
             </tr>
         );
@@ -122,8 +198,10 @@ var OpportunityTable = React.createClass({
         this.props.opportunities.forEach(function(opportunity, i) {
             rows.push(<Opportunity opportunity={opportunity} key={i} />);
         });
-        return (<table className="table-hover" >
-                <thead>
+        return (
+            <div>
+                <table className="table-hover" >
+                    <thead>
                     <tr>
                         <th>Title</th>
                         <th>Time frame</th>
@@ -132,9 +210,15 @@ var OpportunityTable = React.createClass({
                         <th>Category</th>
                         <th>View</th>
                     </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-        </table>);
+                    </thead>
+                    <tbody>{rows}</tbody>
+                </table>
+                <div>
+                    {console.log(rows)}
+                    {rows.length == 0 ? <div>Empty</div> : null}
+                </div>
+            </div>
+        );
     }
 });
 
