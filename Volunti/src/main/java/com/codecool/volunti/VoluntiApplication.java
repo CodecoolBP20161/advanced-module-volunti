d5203d484c2ec4b48b87c26001dfad0402c2f132
lombocska <lombos.monika@gmail.com>
2017-03-07 12:15:36 +0100

    Removed unnecessary imports

--- Volunti/src/main/java/com/codecool/volunti/VoluntiApplication.java
+++ Volunti/src/main/java/com/codecool/volunti/VoluntiApplication.java
@@ -10,11 +10,13 @@ import com.codecool.volunti.service.DataLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableAsync;
 
