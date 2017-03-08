11089d32a7403b669787db1e9cf62ffaab623670
BTamara <bolyatami@gmail.com>
2017-03-08 14:56:46 +0100

    OrganisationController skeleton added

--- Volunti/src/main/java/com/codecool/volunti/controller/OrganisationProfileController.java
+++ Volunti/src/main/java/com/codecool/volunti/controller/OrganisationProfileController.java
@@ -1,13 +1,66 @@
 package com.codecool.volunti.controller;
 
+import com.codecool.volunti.model.Organisation;
+import com.codecool.volunti.repository.OrganisationRepository;
+import com.codecool.volunti.service.*;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class OrganisationProfileController {
 
-    @GetMapping("/organisation")
-    public String renderOrganisationProfile() {
+    private Logger LOGGER = LoggerFactory.getLogger(OrganisationProfileController.class);
+
+    private static EmailType EMAILTYPE = EmailType.CONFIRMATION;
+
+    private OrganisationRepository organisationRepository;
+    private OrganisationService organisationService;
+    private UserService userService;
+    private EmailService emailService;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public OrganisationProfileController(OrganisationRepository organisationRepository,
+                                  OrganisationService organisationService,
+                                  UserService userService,
+                                  EmailService emailService) {
+        this.organisationRepository = organisationRepository;
+        this.organisationService = organisationService;
+        this.userService = userService;
+        this.emailService = emailService;
+    }
+
+    @GetMapping(value = "/profile/organisation")
+    public String renderOrganisationProfile(Model model) {
+        log.info("renderOrganisationRProfile() method called ...");
+
+        Organisation organisation = new Organisation();
+        model.addAttribute("organisation", organisation);
         return "profiles/organisation";
+    }
+
+    @PostMapping( value = "/profile/organisation/saveText")
+    public String saveText(Organisation organisation){
+        log.info("saveText() method called ...");
+
+        organisationService.saveOrganisation(organisation);
+        return "profiles/organisation";  //TODO: What do we want to return here?!
+    }
+
+    @PostMapping( value = "/profile/organisation/saveImage")
+    public String saveImage(Organisation organisation){
+        log.info("saveImage() method called...");
+
+        //TODO: some magic image saving process
+
+        return "profiles/organisation";  //TODO: What do we want to return here?!
     }
 }
