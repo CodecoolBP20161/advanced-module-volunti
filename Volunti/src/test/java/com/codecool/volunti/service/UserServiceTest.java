7d26cc238d7c83e84afc330bf6758417823a84f8
ppayter-cc <pomsarpeter@gmail.com>
2017-03-17 11:54:09 +0100

    UserServiceTest fixed - plz run tests before commit...

--- Volunti/src/test/java/com/codecool/volunti/service/UserServiceTest.java
+++ Volunti/src/test/java/com/codecool/volunti/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.codecool.volunti.service;
 
 import com.codecool.volunti.model.User;
 import com.codecool.volunti.model.enums.UserStatus;
+import com.codecool.volunti.repository.UserRepository;
 import com.codecool.volunti.service.model.UserService;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.junit.After;
@@ -9,7 +10,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.jdbc.JdbcTestUtils;
 
 import java.util.UUID;
 
@@ -19,7 +19,10 @@ public class UserServiceTest extends AbstractServiceTest {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
     private User user1;
+    private User user2;
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -31,35 +34,37 @@ public class UserServiceTest extends AbstractServiceTest {
     public void setUp() {
         user1 = new User("firstName", "lastName", "email@email.com", "password", null, null);
         userService.saveUser(user1);
+
+        user2 = new User("firstName2", "lastName2", "email2@email.com", "password2", null, null);
     }
 
     @After
      public void tearDown() {
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
+//        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
+        userRepository.delete(user1);
+        userRepository.delete(user2);
     }
 
     @Test
-    public void test_saveUser_Should_Return_True() throws Exception {
-        User user2 = new User("firstName2", "lastName2", "email2@email.com", "password2", null, null);
+    public void test_saveUser_shouldBeEqual() throws Exception {
         userService.saveUser(user2);
-
         assertEquals(user2.toString(), userService.getByEmail("email2@email.com").toString());
     }
 
     @Test
-    public void test_getByEmailHappyPath_Should_Return_True() throws Exception {
+    public void test_getByEmailHappyPath_shouldBeEqual() throws Exception {
         User testUser = userService.getByEmail("email@email.com");
-        assertEquals(testUser, user1);
+        assertEquals(user1, testUser);
     }
 
     @Test
-    public void test_getByEmailNoUserFound_Should_Return_True() throws Exception {
+    public void test_getByEmailNoUserFound_shouldReturnNull() throws Exception {
         User testUser = userService.getByEmail("fakemail@email.com");
-        assertEquals(testUser, null);
+        assertEquals(null, testUser);
     }
 
     @Test
-    public void test_getByActivationIDHappyPath_Should_Return_True() throws Exception {
+    public void test_getByActivationIDHappyPath_shouldReturnNull() throws Exception {
         String testID = user1.getActivationID();
         User testUser = userService.getByActivationID(testID.toString());
 
@@ -67,17 +72,17 @@ public class UserServiceTest extends AbstractServiceTest {
     }
 
     @Test
-    public void test_getByActivationIDWrongIDFormat_Should_Return_True() throws Exception {
+    public void test_getByActivationIDWrongIDFormat_shouldReturnNull() throws Exception {
         assertEquals(null, userService.getByActivationID("fakeID"));
     }
 
     @Test
-    public void test_getByActivationIDNoSuchID_Should_Return_True() throws Exception {
+    public void test_getByActivationIDNoSuchID_shouldReturnNull() throws Exception {
         assertEquals(null, userService.confirmRegistration(UUID.randomUUID().toString()));
     }
 
     @Test
-    public void test_confirmRegistrationHappyPath_Should_Return_True() throws Exception {
+    public void test_confirmRegistrationHappyPath_shouldBeActive() throws Exception {
         String testID = user1.getActivationID().toString();
         userService.confirmRegistration(testID);
         User testUser = userService.getByActivationID(testID);
@@ -86,19 +91,19 @@ public class UserServiceTest extends AbstractServiceTest {
     }
 
     @Test
-    public void test_confirmRegistrationWrongIdFormat_Should_Return_True() throws Exception {
+    public void test_confirmRegistrationWrongIdFormat_shouldReturnNull() throws Exception {
         userService.confirmRegistration("fakeID");
         assertEquals(null, userService.confirmRegistration("fakeID"));
 
     }
 
     @Test
-    public void test_confirmRegistrationNoSuchId_Should_Return_True() throws Exception {
+    public void test_confirmRegistrationNoSuchId_shouldReturnNull() throws Exception {
         assertEquals(null, userService.confirmRegistration(UUID.randomUUID().toString()));
     }
 
     @Test
-    public void test_confirmRegistrationUserIsAlreadyActive_Should_True() throws Exception {
+    public void test_confirmRegistration_userIsAlreadyActive_shouldReturnNull() throws Exception {
         String testID = user1.getActivationID().toString();
         userService.confirmRegistration(testID);
         User testUser = userService.getByActivationID(testID);
@@ -106,8 +111,8 @@ public class UserServiceTest extends AbstractServiceTest {
     }
 
     @Test
-    public void test_confirmRegistrationUserIsDisabled_Should_True() throws Exception {
-        User user2 = new User("firstName2", "lastName2", "email2@email.com", "password2", null, null);
+    public void test_confirmRegistrationUserIsDisabled_shouldReturnNull() throws Exception {
+        user2 = new User("firstName2", "lastName2", "email2@email.com", "password2", null, null);
         String testID = user2.getActivationID().toString();
         user2.setUserStatus(UserStatus.DISABLED);
         userService.saveUser(user2);
