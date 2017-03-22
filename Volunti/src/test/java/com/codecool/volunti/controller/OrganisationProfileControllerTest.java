2b050cc93a5a7fbbf2d226a00d58711911199ba5
ppayter-cc <pomsarpeter@gmail.com>
2017-03-22 10:41:52 +0100

    OrganisationProfileController.serveText uses name instead of id, unit test for happy path implemented

---
+++ Volunti/src/test/java/com/codecool/volunti/controller/OrganisationProfileControllerTest.java
@@ -0,0 +1,45 @@
+package com.codecool.volunti.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrganisationProfileControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void renderOrganisationProfile() throws Exception {
+
+    }
+
+    @Test
+    public void serveFile() throws Exception {
+
+    }
+
+    @Test
+    public void serveText() throws Exception {
+
+    }
+
+    @Test
+    public void saveText() throws Exception {
+
+    }
+
+    @Test
+    public void saveImage() throws Exception {
+
+    }
+
+}
