7525867866987f0188dacbf4b0f58f24fc3fd091
lombocska <lombos.monika@gmail.com>
2017-03-28 13:46:38 +0200

    serveText method test is written, it is not OK, the response is 302

--- Volunti/src/test/java/com/codecool/volunti/controller/OrganisationProfileEditingWorkflowTest.java
+++ Volunti/src/test/java/com/codecool/volunti/controller/OrganisationProfileEditingWorkflowTest.java
@@ -2,7 +2,40 @@ package com.codecool.volunti.controller;
 
 
 import com.codecool.volunti.service.AbstractServiceTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.annotation.Resource;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class OrganisationProfileEditingWorkflowTest extends AbstractServiceTest {
+
+    @Resource
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void test_renderOrganisationProfile_GET() throws Exception {
+        this.mockMvc.perform(get("/profile/organisation")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("profiles/organisation"));
+    }
 
 }
