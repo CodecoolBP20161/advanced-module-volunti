1253389ab51e05c7aaf1da565a852d6159afa636
lombocska <lombos.monika@gmail.com>
2017-03-29 11:36:49 +0200

    Added test case for rendered view status OK with logged in user

--- Volunti/src/test/java/com/codecool/volunti/controller/OrganisationProfileEditingWorkflowTest.java
+++ Volunti/src/test/java/com/codecool/volunti/controller/OrganisationProfileEditingWorkflowTest.java
@@ -4,6 +4,10 @@ package com.codecool.volunti.controller;
 import com.codecool.volunti.service.AbstractServiceTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -11,16 +15,18 @@ import org.springframework.web.context.WebApplicationContext;
 import javax.annotation.Resource;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 public class OrganisationProfileEditingWorkflowTest extends AbstractServiceTest {
 
     @Resource
     private WebApplicationContext webApplicationContext;
-
     private MockMvc mockMvc;
 
     @Before
@@ -31,11 +37,11 @@ public class OrganisationProfileEditingWorkflowTest extends AbstractServiceTest 
     }
 
     @Test
-    public void test_renderOrganisationProfile_GET() throws Exception {
+    public void test_renderOrganisationProfile_GET_Should_Render_Page() throws Exception {
         this.mockMvc.perform(get("/profile/organisation")
+                .with(user("test@gmail.com").password("password").roles("USER"))
                 .with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("profiles/organisation"));
     }
-
 }
