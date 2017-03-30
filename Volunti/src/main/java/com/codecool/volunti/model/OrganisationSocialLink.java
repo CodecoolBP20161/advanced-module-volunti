f36b059ee52652cf013d3b852f09fb2782043790
lombocska <lombos.monika@gmail.com>
2017-03-30 11:11:42 +0200

    Added basic model with the foreign key relationship to bind the organisation with some social link

---
+++ Volunti/src/main/java/com/codecool/volunti/model/OrganisationSocialLink.java
@@ -0,0 +1,32 @@
+package com.codecool.volunti.model;
+
+
+import com.codecool.volunti.model.enums.SocialLink;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="organisation_social_link")
+@Data
+@Slf4j
+public class OrganisationSocialLink {
+
+    @Id
+    @Column(name="social_id", unique=true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="social_link_type")
+    @Enumerated(EnumType.STRING)
+    private SocialLink socialLink;
+
+    @Column(name="social_link_url")
+    private String socialLinkUrl;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="organisation_id")
+    private Organisation organisation_social_link;
+
+}
