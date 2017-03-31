34fdda65a7da083d6bdf8d821ff64059e6ae14a9
ppayter-cc <pomsarpeter@gmail.com>
2017-03-31 14:23:58 +0200

    SocialLinkController is using logged-in user via Principal

--- Volunti/src/main/java/com/codecool/volunti/controller/SocialLinkController.java
+++ Volunti/src/main/java/com/codecool/volunti/controller/SocialLinkController.java
@@ -1,26 +1,36 @@
 package com.codecool.volunti.controller;
 
 
+import com.codecool.volunti.model.Organisation;
 import com.codecool.volunti.model.OrganisationSocialLink;
+import com.codecool.volunti.model.User;
 import com.codecool.volunti.service.model.OrganisationService;
+import com.codecool.volunti.service.model.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
+
 
 @RestController
 public class SocialLinkController {
 
-    OrganisationService organisationService;
+    private OrganisationService organisationService;
+    private UserService userService;
 
     @Autowired
-    public SocialLinkController(OrganisationService organisationService) {
+    public SocialLinkController(OrganisationService organisationService, UserService userService) {
         this.organisationService = organisationService;
+        this.userService = userService;
     }
 
     @GetMapping(value = "/profile/organisation/social-links")
-    public List<OrganisationSocialLink> serveSocialLink(){
-        return organisationService.findByOrganisationId(organisationService.get(1));
+    public List<OrganisationSocialLink> serveSocialLink(Principal principal){
+        User user = userService.getByEmail(principal.getName());
+        Organisation organisation = user.getOrganisation();
+
+        return organisationService.findByOrganisationId(organisation);
     }
 }
