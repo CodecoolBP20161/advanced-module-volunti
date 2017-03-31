349541c33f3e4b8ad19a3430bdbaa3ec4697a205
lombocska <lombos.monika@gmail.com>
2017-03-31 14:05:34 +0200

    Switched to OrganisationService instead of OrganisationSocialLinkService

--- Volunti/src/main/java/com/codecool/volunti/controller/SocialLinkController.java
+++ Volunti/src/main/java/com/codecool/volunti/controller/SocialLinkController.java
@@ -3,7 +3,6 @@ package com.codecool.volunti.controller;
 
 import com.codecool.volunti.model.OrganisationSocialLink;
 import com.codecool.volunti.service.model.OrganisationService;
-import com.codecool.volunti.service.model.OrganisationSocialLinkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
