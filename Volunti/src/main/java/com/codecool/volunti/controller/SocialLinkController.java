8900257a6c263089ccc2161f4954ed573aa2eb5a
ppayter-cc <pomsarpeter@gmail.com>
2017-04-11 11:00:38 +0200

    unnecessary/unused serveSocialLink method removed  from SocialLinkController

--- Volunti/src/main/java/com/codecool/volunti/controller/SocialLinkController.java
+++ Volunti/src/main/java/com/codecool/volunti/controller/SocialLinkController.java
@@ -1,17 +1,10 @@
 package com.codecool.volunti.controller;
 
 
-import com.codecool.volunti.model.Organisation;
-import com.codecool.volunti.model.OrganisationSocialLink;
-import com.codecool.volunti.model.User;
 import com.codecool.volunti.service.model.OrganisationService;
 import com.codecool.volunti.service.model.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
-import java.util.List;
 
 
 @RestController
@@ -24,13 +17,5 @@ public class SocialLinkController {
     public SocialLinkController(OrganisationService organisationService, UserService userService) {
         this.organisationService = organisationService;
         this.userService = userService;
-    }
-
-    @GetMapping(value = "/profile/organisation/social-links")
-    public List<OrganisationSocialLink> serveSocialLink(Principal principal){
-        User user = userService.getByEmail(principal.getName());
-        Organisation organisation = user.getOrganisation();
-
-        return organisationService.findByOrganisationId(organisation);
     }
 }
